f6c45b5a80c9e2ff74682da3504f1c53046060c4
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-10-16 22:31:20 +0300

    Add html-leader and generators for assets

--- webpack.config.js
+++ webpack.config.js
@@ -50,15 +50,29 @@ const config = {
         test: /\.css$/i,
         use: [stylesHandler, 'css-loader', 'postcss-loader'],
       },
+
       // img rules
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
       },
+
+      // html rules
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+
       // font rules
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
       },
 
       // babel rules
